79db5f5e9656bcd0e9fb173b685686a80fca595f
Chartok <m.bhimjee@outlook.com>
2023-06-24 23:11:11 -0500

    renamed models files

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// Post belongs to User
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// Post has many Comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+// Comment belongs to User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// User has many Posts
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
