362493b24b45a631bf466f657d50149dffd09723
Chartok <m.bhimjee@outlook.com>
2023-06-25 01:55:00 -0500

    working through errors

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const helpers = require('/utils/helpers');
 const dotenv = require('dotenv').config();
 
 dotenv.config();
