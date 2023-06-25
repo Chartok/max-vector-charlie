196055abda29b225e43b63bc80640113aba04b52
Chartok <m.bhimjee@outlook.com>
2023-06-25 02:18:55 -0500

    remodling

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
-const { Model, DataTypes } = require('sequelize');
 
 class User extends Model {
     // Set up method to run on instance data (per user) to check password
