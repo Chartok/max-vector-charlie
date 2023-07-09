18c9fbc11baaf9f3134eafc6463ffff30f471266
Chartok <m.bhimjee@outlook.com>
2023-07-09 17:37:49 -0500

    regroup; server.js

---
+++ wut/server.js
@@ -0,0 +1,10 @@
+// const { app, PORT } = require('./middleware');
+// const { sesh } = require('./session');
+
+// require('./handlebars');
+// require('../controllers');
+
+// app.listen(PORT, () => {
+//     console.log(`App listening on ${PORT}!`);
+//     sequelize.sync({ force: false });
+// });
---
+++ wut/session.js
@@ -0,0 +1,23 @@
+// const session = require('express-session');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const sequelize = require('../config/connection');
+
+// const sessionSecret = process.env.SESSION_SECRET;
+// const sesh = {
+//     secret: sessionSecret,
+//     cookie: {
+//         // Session will automatically expire in 10 minutes
+//         expires: 10 * 60 * 1000,
+//         httpOnly: true,
+//         secure: false,
+//         maxAge: null,
+//         sameSite: 'strict'
+//     },
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
+
+// module.exports = { sesh };
