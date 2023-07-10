85661c16202d8de9149bbe47b3de8324b363a3f5
Chartok <m.bhimjee@outlook.com>
2023-07-10 15:18:06 -0500

    restructured post new user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,22 +2,22 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // POST new user to the database
-router.post('/signup', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        // Hash the new user's password and store the resulting hash in the database
-        req.body.password = await bcrypt.hash(req.body.password, 10);
-        // Create a new user
+        // Create new user
         const newUser = await User.create({
             username: req.body.username,
             password: req.body.password,
         });
+        // Hash the new user's password and store the resulting hash in the database
+        req.body.password = await bcrypt.hash(req.body.password, 10);
         // Set up sessions with a 'loggedIn' variable set to `true`
         const userData = await setSession(req, newUser);
         res.status(200).json({ message: 'User is registered!', userData })
 
     } catch (error) {
-        console.log('There was an error signing up');
-        res.status(500).json(error);
+        console.error('There was an error signing up');
+        throw error;
     }
 });
 
@@ -32,8 +32,7 @@ router.post('/login', async (req, res) => {
         });
         // If there is no user with that username or password is incorrect, return an error message
         if (!userData || !(await bcrypt.compare(req.body.password, userData.password))) {
-            res.status(401).json({ message: 'Incorrect username or password, please try again' });
-            return;
+            throw new Error('Incorrect username or password, please try again');
         }
         // If the user is found, compare the entered password with the password hash stored in the database
         const validPassword = await bcrypt.compare(req.body.password, userData.password);
