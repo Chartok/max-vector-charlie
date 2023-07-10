a6b06b155b1f213bfdebcab681e5778278d1e388
Chartok <m.bhimjee@outlook.com>
2023-07-10 16:54:40 -0500

    refactored to arrow function for consistency

--- utils/session-helper.js
+++ utils/session-helper.js
@@ -1,11 +1,12 @@
-function setSession(req, userData) {
+setSession = async (req, userData) => {
     return new Promise((resolve, reject) => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
             req.session.username = userData.username;
             req.session.save(error => {
                 if (error) {
-                    console.log(error);
+                    console.error('There was an error saving the session');
+                    console.log(session)
                     reject(error);
                 } else {
                     resolve(req.session);
