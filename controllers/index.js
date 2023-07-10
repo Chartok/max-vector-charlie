8d26041ceb8bab90a203eb8aaabe71624c9c92e7
Chartok <m.bhimjee@outlook.com>
2023-07-10 18:52:18 -0500

    added dashboard routes to controller index

--- controllers/index.js
+++ controllers/index.js
@@ -3,19 +3,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const mainRoutes = require('./main-routes');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 
 router.use('/api/', apiRoutes);
 router.use('/', mainRoutes);
-
-// Handlebars routes
-router.get('/', async (req, res) => {
-    try {
-        res.render('dashboard', { layout: 'dashboard' });
-    } catch (error) {
-        console.log('There was an error getting to the dashboard');
-        res.statusCode.json(error);
-    }
-});
+router.use('/dashboard', dashboardRoutes)
 
 module.exports = router;
